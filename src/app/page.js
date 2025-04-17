7f28bba1c6fda2e66efe9a72f9d598609c7659d1
chshafiq70 <arhamchaudhary424@gmail.com>
2025-04-17 20:55:15 +0500

    add hea1

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Activities from "./components/activity";
 import BlogSection from "./components/blog";
 import FacilityDetails from "./components/facilties";
 import FeaturedSites from "./components/feature";
-import HeroSection from "./components/hero";
+import HeroSection from './components/HeroSection';
 import TestimonialImageSection from "./components/image";
 import InstagramSlider from "./components/instagram";
 import TestimonialSlider from "./components/testimonial";
